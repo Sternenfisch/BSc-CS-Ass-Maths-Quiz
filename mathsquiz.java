3c7adf2ebcdfd44b3c57d4e7a81404df69e72f61
Robert Boettcher <sternen.fish@googlemail.com>
2016-06-14 00:34:33 +0500

    Create mathsquiz.java

---
+++ mathsquiz.java
@@ -0,0 +1,23 @@
+
+public class mathsquiz {
+	//<@}-|-|-- SF --|-|-{@>
+	//Parameter arguments
+	
+	
+	public static void main(String[] args){
+		
+        
+		System.out.println("  88b           d88                       88                           ,ad8888ba,                 88             ");
+		System.out.println("  888b         d888                ,d     88                          d8''    `'8b                ''             ");
+		System.out.println("  88`8b       d8'88                88     88                         d8'        `8b                              ");
+		System.out.println("  88 `8b     d8' 88  ,adPPYYba,  MM88MMM  88,dPPYba,   ,adPPYba,     88          88  88       88  88  888888888  ");
+		System.out.println("  88  `8b   d8'  88  ''     `Y8    88     88P'    '8a  I8[    ''     88          88  88       88  88       a8P'  ");
+		System.out.println("  88   `8b d8'   88  ,adPPPPP88    88     88       88   `'Y8ba,      Y8,    '88,,8P  88       88  88    ,d8P'    ");
+		System.out.println("  88    `888'    88  88,    ,88    88,    88       88  aa    ]8I      Y8a.    Y88P   '8a,   ,a88  88  ,d8'       ");
+		System.out.println("  88     `8'     88  `'8bbdP'Y8    'Y888  88       88  `'YbbdP''       `'Y8888Y'Y8a   `'YbbdP'Y8  88  888888888  ");
+        
+        
+
+
+	}
+}
